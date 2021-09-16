c3a4e7a5c514f08dbec6d037e382b89779bcd185
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2021-09-16 15:22:58 -0400

    Update and rename debian10config.json to debian10cse.sh

---
+++ linux/omi/debian10cse.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add MSRepo
+sudo curl -sSL https://packages.microsoft.com/keys/microsoft.asc
+sudo apt-key add -
+sudo apt-add-repository https://packages.microsoft.com/debian/10/prod
+sudo apt-get update
+
+# Install omi release
+sudo apt-get install -y omi
